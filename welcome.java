365e288d58cc429800fb6e6c2a1c083528c4dc5a
Cassi <65311088+cmason2394@users.noreply.github.com>
2023-08-01 15:48:23 -0600

    Java class assignments

---
+++ welcome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class welcome {
+    public static void main(String [] args) {
+        Scanner scnr = new Scanner(System.in);
+        String userName;
+
+        System.out.println("Please enter your username: ");
+        userName = scnr.next();
+
+        System.out.println("Hello " + userName + ", and welcome to CS Online!");
+    }
+}
