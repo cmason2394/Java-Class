79ea48c2e9448c30b8955af7f162f9b007a98988
Cassi <65311088+cmason2394@users.noreply.github.com>
2023-08-02 08:20:35 -0600

    start leap year program

---
+++ LeapYear.java
@@ -0,0 +1,12 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int inputYear;
+        boolean isLeapYear;
+
+        isLeapYear = false;
+        inputYear = scnr.nextInt();
+
+        
+    }
+}
