dcf210545384cc47940d11667ff308d866889475
Cassi <cassi2394@gmail.com>
2023-08-17 13:59:28 -0600

    get input method

--- FilterNumberList.java
+++ FilterNumberList.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class FilterNumberList {
     public static void getUserValues(int[] array, int arrSize, Scanner scnr) {
-
+        for (int i = 0; i < arrSize; i++) {
+            array[i] = scnr.nextInt();
+        }
     }
 
     public static void outputIntsLessThanOrEqualToThreshold(int[] array, int arrSize, int threshold) {
-        
+        for (int i = 0; i < arrSize; i++) {
+            if (array[i] <)
+        }
     }
     public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
