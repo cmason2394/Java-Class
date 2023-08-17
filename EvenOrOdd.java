141bb6c319b795c22b8ef13f5ba8a4113ec0ca06
Cassi <cassi2394@gmail.com>
2023-08-17 14:39:01 -0600

    start of program to determine if a list is even or odd or neither

---
+++ EvenOrOdd.java
@@ -0,0 +1,28 @@
+public class EvenOrOdd {
+    /**
+     * Determines if an inputed list of integers contains all even numbers,
+     * all odd numbers, or a mix of both
+     * @param arrayValues array of user input integers
+     * @param arraySize length of the array
+     * 
+     */
+    public static boolean isArrayEven(int[] arrayValues, int arraySize) {
+        /**
+         * Determine if every element in an array is even
+         * @return boolean value, true if all even, false if not
+         */
+    }
+
+    public static boolean isArrayOdd(int[] arrayValues, int arraySize) {
+        /**
+         * Determine if every element in an array is odd
+         * @return boolean value, true if all odd, false if not
+         */
+    }
+
+    public static void main(String[] args) {
+        /**
+         * 
+         */
+    }
+}
