1a5469f05731fa825a7a6b19cd5da128e16a57f5
Cassi <cassi2394@gmail.com>
2023-08-22 08:17:00 -0600

    first commit, track frequency of word use

---
+++ WordFrequency.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class WordFrequency {
+    public static int getFrequencyOfWord(String[] wordsList, int listSize, String currWord) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
