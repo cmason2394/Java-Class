93bd159335e0f3f3f25fc2abd35aa14d7d8f5f69
Cassi <cassi2394@gmail.com>
2023-08-24 14:47:27 -0600

    comments

--- NutritionalInfo.java
+++ NutritionalInfo.java
@@ -4,6 +4,7 @@ public class NutritionalInfo {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       
+      //foodItem object with no initial values, calls on default constructor
       FoodItem foodItem1 = new FoodItem();
       
       System.out.println("What food are you eating?");
@@ -15,6 +16,7 @@ public class NutritionalInfo {
       System.out.println("How many grams of protein does one serving contain?");
       double amountProtein = scnr.nextDouble();
       
+      //foodItem object with initial values, calls on second constructor
       FoodItem foodItem2 = new FoodItem(itemName, amountFat, amountCarbs, amountProtein);
       
       System.out.print("Enter number of servings: ");
@@ -23,12 +25,15 @@ public class NutritionalInfo {
 
       scnr.close();
       
+      //calls on printInfo of first fooditem object, default constructor initialized values to "None" for name and 0 for all other values
+      //which will be printed by printInfo
       foodItem1.printInfo();
       System.out.printf("Number of calories for %.2f serving(s): %.2f\n", numServings, 
                           foodItem1.getCalories(numServings));
                            
       System.out.println("\n");
-                           
+        
+      //calls on printInfo of second fooditem object, constructor initialized inputted values, which will be printed by printInfo
       foodItem2.printInfo();
       System.out.printf("Number of calories for %.2f serving(s): %.2f\n", numServings,
                           foodItem2.getCalories(numServings));
