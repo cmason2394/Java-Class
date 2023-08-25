4a8c125f5c28c7419f4606987bf68bd0ecbc1b18
Cassi <cassi2394@gmail.com>
2023-08-25 10:37:31 -0600

    comments explaining connection between BookInformation, Book, and Encyclopedia

---
+++ Encyclopedia.java
@@ -0,0 +1,27 @@
+public class Encyclopedia extends Book {
+    /*
+     * Derived class from Book class
+     * Additional data members of encyclopedia edition and number of volumes
+     * sets and gets edition and number of volumes
+     * Overrides book print method to also print the edition and number of volumes
+     */
+   // TODO: Declare private fields: edition, numVolumes
+   private int edition;
+   private int numVolumes;
+  
+   
+   // TODO: Define mutator methods - 
+   //       setEdition(), setNumVolumes()
+   public void setEdition(int userEdition) {
+    
+   }
+   
+  
+   // TODO: Define accessor methods -
+   //       getEdition(), getNumVolumes()
+   
+   
+   // TODO: Define a printInfo() method that overrides 
+   //       the printInfo in Book class 
+   
+}
