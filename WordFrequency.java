bed4c0de9b900e854f47c7c6295492a4c606ca30
Cassi <cassi2394@gmail.com>
2023-08-25 12:27:44 -0600

    removed troubleshooting comments and variable tracking

--- WordFrequency.java
+++ WordFrequency.java
@@ -10,19 +10,9 @@ public class WordFrequency {
          *for loop that compares currWord to every word in the array
          *and increases wordCount if they match
         */
-        //loop is only executing once rather than going through entire array
-        //i starts and ends as the same value as the i for the loop that is calling it in main
-        //I have tried changing the "inner i" to j, doesn't change
-        //listLength doesn't change, so I don't know why the loop stops before it gets to its stopping condition
-        //oh, it is going through the loop, but the condition of the if statement not working correctly
-        //if statement not recognizing when 2 words are the same...
-        //Lesson learned: for strings, compare with method equals() rather than equality operator
         for (int i = 0; i < listSize; i++) {
-            System.out.println("   inner i: " + i);
-            System.out.println("      " + currWord.equals(wordsList[i]));
             if (currWord.equals(wordsList[i])) {
                 wordCount++;
-                System.out.println("      if statement inner i: " + i);
             }
         }
 
@@ -49,7 +39,6 @@ public class WordFrequency {
 
         //for loop to compare compareWord to every word in the array
         for (int i = 0; i< LENGTH; i++) {
-            System.out.println("outer i: " + i);
             compareWord = userWords[i];
             frequencies[i] = getFrequencyOfWord(userWords, LENGTH, compareWord);
         }
