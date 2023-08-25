cb3029a7ef079c44add2e1be1b52d8a6e3dcba41
Cassi <cassi2394@gmail.com>
2023-08-25 12:20:16 -0600

    troubleshooting WordFrequency program. All comments and tracking variables included.

--- WordFrequency.java
+++ WordFrequency.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
 
 public class WordFrequency {
+    //method to determine how often a word in an array occurs
     public static int getFrequencyOfWord(String[] wordsList, int listSize, String currWord) {
+        //initiate variables
         int wordCount = 0;
         
+        /*
+         *for loop that compares currWord to every word in the array
+         *and increases wordCount if they match
+        */
+        //loop is only executing once rather than going through entire array
+        //i starts and ends as the same value as the i for the loop that is calling it in main
+        //I have tried changing the "inner i" to j, doesn't change
+        //listLength doesn't change, so I don't know why the loop stops before it gets to its stopping condition
+        //oh, it is going through the loop, but the condition of the if statement not working correctly
+        //if statement not recognizing when 2 words are the same...
+        //Lesson learned: for strings, compare with method equals() rather than equality operator
         for (int i = 0; i < listSize; i++) {
-            if (currWord == wordsList[i]) {
+            System.out.println("   inner i: " + i);
+            System.out.println("      " + currWord.equals(wordsList[i]));
+            if (currWord.equals(wordsList[i])) {
                 wordCount++;
-                System.out.println("current word: " + currWord);
-                System.out.println("compared to " + wordsList[i]);
-                System.out.println("word count: " + wordCount);
+                System.out.println("      if statement inner i: " + i);
             }
         }
 
@@ -20,12 +33,13 @@ public class WordFrequency {
         //initiate variables
         Scanner scnr = new Scanner(System.in);
         System.out.println("How many words will you enter?");
-        int listLength = scnr.nextInt();
-        String[] userWords = new String[listLength];
+        final int LENGTH = scnr.nextInt();
+        String[] userWords = new String[LENGTH];   //array of user words
         String compareWord = "";
-        int frequency = 0;
+        int[] frequencies = new int[LENGTH];   //array of frequency of each word
 
-        for (int i = 0; i < listLength; i++) {
+        //for loop to enter user words into an array
+        for (int i = 0; i < LENGTH; i++) {
             System.out.print("Enter next word: ");
             userWords[i] = scnr.next();
             System.out.println();
@@ -33,13 +47,16 @@ public class WordFrequency {
 
         scnr.close();
 
-        compareWord = userWords[0];
-
-        while (compareWord <= userWords[listLength - 1]) {
-            frequency = getFrequencyOfWord(userWords, listLength, compareWord);
-            System.out.println(compareWord + " " + frequency);
-            ++i;
+        //for loop to compare compareWord to every word in the array
+        for (int i = 0; i< LENGTH; i++) {
+            System.out.println("outer i: " + i);
             compareWord = userWords[i];
+            frequencies[i] = getFrequencyOfWord(userWords, LENGTH, compareWord);
+        }
+
+        //for loop to print each word and its frequency
+        for (int i = 0; i < LENGTH; i++) {
+            System.out.println(userWords[i] + " " + frequencies[i]);
         }
 
     }
